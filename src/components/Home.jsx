4e7f1c77195b7c5bb0c5b3810981581fe6463ff4
Vidal Graupera <vgraupera@gmail.com>
2017-02-20 19:24:27 -0800

    fix(Home.jsx): reset title when returning to home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
+
+import * as headerActions from '../actions/header';
 import { openAuth } from '../actions/auth';
 
-const Home = (props) => {
-  return (
-    <div className="container home">
-      <p>
-        Welcome, this is a one on one tracking app for managers and their direct reports.
-      </p>
-      <p>
-        Create a free account using Google
-      </p>
-      <RaisedButton
-        primary={true}
-        label="Sign up"
-        onTouchTap={props.openAuth}
-      />
-    </div>
-  );
-};
+class Home extends Component {
+  componentDidMount() {
+    this.props.setText('1on1 Tracker');
+  }
+
+  render() {
+    return (
+      <div className="container home">
+        <p>
+          Welcome, this is a one on one tracking app for managers and their direct reports.
+        </p>
+        <p>
+          Create a free account using Google
+        </p>
+        <RaisedButton
+          primary={true}
+          label="Sign up"
+          onTouchTap={this.props.openAuth}
+        />
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = {
   openAuth,
+  setText: headerActions.setText,
 };
 
 export default connect(null, mapDispatchToProps)(Home);
