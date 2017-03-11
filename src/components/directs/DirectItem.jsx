7024f2b3215f8d6520ba6c6570182a6d1b08d59c
Vidal Graupera <vgraupera@gmail.com>
2017-03-11 07:55:45 -0800

    feature(DirectItem): show simple avatar in directs list

--- src/components/directs/DirectItem.jsx
+++ src/components/directs/DirectItem.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { ListItem } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+
+const initials = (name) => {
+    return name.split(' ').map((w) => w[0]).join('');
+}
 
 const DirectItem = props => (
   <ListItem
     primaryText={props.direct.name}
+    secondaryText={props.direct.phone}
     containerElement={<Link to={`/directs/${props.id}`} />}
+    leftAvatar={<Avatar>{initials(props.direct.name)}</Avatar>}
   />
 );
 
