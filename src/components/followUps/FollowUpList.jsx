2cd4827a4fc44c146ac563e5e52410d2a88867fe
Vidal Graupera <vgraupera@gmail.com>
2017-03-13 13:21:58 -0700

    feature(FollowUpList): hide/show completed items

--- src/components/followUps/FollowUpList.jsx
+++ src/components/followUps/FollowUpList.jsx
@@ -12,14 +12,36 @@ import FollowUpItem from './FollowUpItem';
 import * as HeaderActions from '../../actions/header';
 
 class FollowUpList extends Component {
+  constructor() {
+    super();
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+    this.state = {
+      showCompleted: false,
+    };
+  }
+
   componentDidMount() {
     this.props.setText('Follow Ups');
   }
 
+  toggleCompleted() {
+    this.setState({ showCompleted: !this.state.showCompleted });
+  }
+
   renderFollowUps() {
     const rows = [];
+    rows.push(
+      <ListItem
+        primaryText={this.state.showCompleted ? 'Hide Completed' : 'Show Completed'}
+        onTouchTap={this.toggleCompleted}
+      />,
+    );
+
     if (this.props.followUps && this.props.followUps.size > 0) {
       this.props.followUps.forEach((followUp, key) => {
+        if (!this.state.showCompleted && followUp.completed) {
+          return;
+        }
         rows.push(
           <FollowUpItem
             key={key}
