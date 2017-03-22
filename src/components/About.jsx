0db5a3a9afa292125a504295a9ce51cc41e61710
Vidal Graupera <vgraupera@gmail.com>
2017-03-21 23:20:05 -0700

    feature(about): add something to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as headerActions from '../actions/header';
 
+import { brightGreen } from '../colors';
+
+const style = {
+  title: {
+    color: brightGreen,
+    fontSize: '1.5em',
+  },
+};
+
 class About extends Component {
   componentDidMount() {
     this.props.setText('About');
@@ -10,7 +19,15 @@ class About extends Component {
   render() {
     return (
       <div className="container about">
-        <p>Hi, this is the about page. Not much here yet.</p>
+        <h1 style={style.title}>1on1tracker.com</h1>
+        <p>If you find this useful, contact or follow
+          me <a href="https://twitter.com/vgraupera">@vgraupera</a> or
+          on <a href="https://www.linkedin.com/in/vidalgraupera/">linkedin</a>.
+        </p>
+        <p>
+          Source code on <a href="https://github.com/VGraupera/1on1tracker">
+            github</a>
+        </p>
       </div>
     );
   }
