34845ecd00e3d7ab81fa9b904ebb4e4236bcad75
Vidal Graupera <vgraupera@gmail.com>
2017-03-27 11:27:07 -0700

    chore(index.css): add sass pre-processor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import ArrowIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-up';
 
 const Footer = () => (
   <div className="footer">
@@ -11,9 +12,9 @@ const Footer = () => (
         <Link to="/privacy">Privacy Policy</Link>
       </li>
     </ul>
-    <p class="small">Use of this website signifies your agreement to the Terms and Conditions and Privacy Policy.</p>
+    <p>Use of this website signifies your agreement to the Terms and Conditions and Privacy Policy.</p>
     <div id="toplink">
-      <a href="#" class="top-link" title="Back to top">Back To Top <i class="fa fa-chevron-up"></i></a>
+      <a href="#" className="top-link" title="Back to top">Back To Top <ArrowIcon color="#fff" /></a>
     </div>
   </div>
 );
